88fa28fb8dddeb530e92f654eea87ff5c101f6e2
skullmind <xujianhuaap@gmail.com>
2021-03-21 10:07:46 -0400

    feature:save to photo

--- app/src/main/java/cn/skullmind/mbp/mymeng/pick_picture/PickPictureActivity.kt
+++ app/src/main/java/cn/skullmind/mbp/mymeng/pick_picture/PickPictureActivity.kt
@@ -4,16 +4,23 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import cn.skullmind.mbp.mymeng.R
+import java.io.File
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.SimpleFormatter
 
 fun startPickPictureActivity(context: AppCompatActivity){
     val intent = Intent(context,PickPictureActivity::class.java)
@@ -23,11 +30,19 @@ fun startPickPictureActivity(context: AppCompatActivity){
 
 class PickPictureActivity:AppCompatActivity() {
     private lateinit var preview:PreviewView
+    private lateinit var btnTakePhoto: View
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pick_picture)
-        preview = findViewById(R.id.preview);
+        preview = findViewById(R.id.preview)
+        btnTakePhoto = findViewById(R.id.camera_capture_button)
+
+        btnTakePhoto.setOnClickListener{
+            takePhoto()
+        }
+
         if(allPermissionsGranted()){
             openCamera()
         }else {
@@ -36,6 +51,26 @@ class PickPictureActivity:AppCompatActivity() {
     }
 
 
+    private fun takePhoto(){
+        val imageCapture = this.imageCapture ?: return
+        val fileName = String.format("%d.jpg",System.currentTimeMillis())
+        val photoFile = File(PHOTO_DIR,SimpleDateFormat("yyyyMMddHHmmss").format(Date())+".jpg")
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(this@PickPictureActivity,"save photo failure",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(this@PickPictureActivity,"save photo success",Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        )
+
+
+    }
     private fun openCamera(){
 
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
@@ -45,11 +80,14 @@ class PickPictureActivity:AppCompatActivity() {
                 it.setSurfaceProvider(this.preview.createSurfaceProvider())
             }
 
-            val cameraIndex = CameraSelector.DEFAULT_BACK_CAMERA;
+            val cameraIndex = CameraSelector.DEFAULT_BACK_CAMERA
 
+            if(this.imageCapture == null){
+                this.imageCapture = ImageCapture.Builder().build()
+            }
             try {
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this,cameraIndex,previewFace)
+                cameraProvider.bindToLifecycle(this,cameraIndex,previewFace,imageCapture)
             }catch (e: Exception){
                 e.printStackTrace();
             }
@@ -79,6 +117,7 @@ class PickPictureActivity:AppCompatActivity() {
     companion object{
         private  val REQUEST_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private const val REQUEST_CODE_CAMERA = 0x1;
+        private const val PHOTO_DIR = "";
 
     }
 }
