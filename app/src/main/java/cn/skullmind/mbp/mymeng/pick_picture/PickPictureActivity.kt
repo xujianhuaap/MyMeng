7dcbc6eed3326677d090687e2578175f2d88c9ea
skullmind <xujianhuaap@gmail.com>
2021-03-21 03:47:08 -0400

    feature:add lifecycle dependncies

---
+++ app/src/main/java/cn/skullmind/mbp/mymeng/pick_picture/PickPictureActivity.kt
@@ -0,0 +1,11 @@
+package cn.skullmind.mbp.mymeng.pick_picture
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PickPictureActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/cn/skullmind/mbp/mymeng/pick_picture/PickPictureViewModel.java
@@ -0,0 +1,12 @@
+package cn.skullmind.mbp.mymeng.pick_picture;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class PickPictureViewModel extends AndroidViewModel {
+    public PickPictureViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
