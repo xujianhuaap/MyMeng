9dcb2489e30aaae4dd9795273d856cbe16a15c10
skullmind <xujianhuaap@gmail.com>
2021-05-28 10:25:45 +0800

    feature:GLActivity for switch surface view

--- app/src/main/java/cn/skullmind/mbp/mymeng/gl/GLActivity.kt
+++ app/src/main/java/cn/skullmind/mbp/mymeng/gl/GLActivity.kt
@@ -3,7 +3,10 @@ package cn.skullmind.mbp.mymeng.gl
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
+import cn.skullmind.mbp.mymeng.R
 import cn.skullmind.mbp.mymeng.gl.second.Gl20SurfaceView
 import cn.skullmind.mbp.mymeng.gl.third.Gl30SurfaceView
 
@@ -13,9 +16,23 @@ fun startGLActivity(context: Context) {
 }
 
 class GLActivity : FragmentActivity() {
+    private lateinit var container:ViewGroup
+    private var isOOD = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val glView = Gl20SurfaceView(this)
-        setContentView(glView)
+        setContentView(R.layout.activity_gl)
+        container = findViewById<ViewGroup>(R.id.container).apply {
+            addView(Gl30SurfaceView(this@GLActivity))
+            addView(Gl20SurfaceView(this@GLActivity))
+        }
+        findViewById<View>(R.id.tv_switch_view).setOnClickListener {
+            getGL20SurfaceView().visibility = if(isOOD)View.VISIBLE else View.GONE
+            getGL30SurfaceView().visibility = if(isOOD)View.GONE else View.VISIBLE
+            isOOD = !isOOD
+        }
     }
+
+    private fun getGL30SurfaceView() = container.getChildAt(1)
+
+    private fun getGL20SurfaceView() = container.getChildAt(0)
 }
