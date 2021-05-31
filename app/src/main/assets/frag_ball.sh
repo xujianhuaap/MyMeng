f2341689adcd6e7654995043b57f72f47f8e2361
skullmind <xujianhuaap@gmail.com>
2021-05-31 16:18:25 +0800

    feature: add ambient light

--- app/src/main/assets/frag_ball.sh
+++ app/src/main/assets/frag_ball.sh
@@ -3,6 +3,7 @@ precision mediump float;
 uniform float uR;
 in vec2 mcLongLat;
 in vec3 aPosition;
+in vec4 vAmbientLight;
 out vec4 fragColor;
 void main(){
    vec3 color;
@@ -13,10 +14,10 @@ void main(){
    int k = int((aPosition.z + uR)/span);
    int whichColor = int(mod(float(i+j+k),2.0));
    if(whichColor == 1) {
-   		color = vec3(0f,0.25f,0f);
+   		color = vec3(0.678,0.231,0.129);
    }
    else {
-   		color = vec3(0.5f,0.3f,0f);
+   		color = vec3(1.0,1.0,1.0);
    }
-   fragColor=vec4(color,1.0f);
+   fragColor=vec4(color,0f)*vAmbientLight;
 }
--- app/src/main/assets/vertex_ball.sh
+++ app/src/main/assets/vertex_ball.sh
@@ -1,8 +1,10 @@
 #version 300 es
 uniform mat4 uMVPMatrix;
 in vec3 vPosition;
+out vec4 vAmbientLight;
 out vec3 aPosition;
 void main(){
   gl_Position = uMVPMatrix * vec4(vPosition,1);
   aPosition = vPosition;
+  vAmbientLight = vec4(0.15,0.15,0.15,1.0);
 }
